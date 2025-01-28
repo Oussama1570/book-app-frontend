784ed93beae0334ed7a7657514247ed4ddd68f86
Oussama <Messamessus@yahoo.com>
2025-01-28 18:05:27 +0100

    update url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "https://book-app-backend-gules-nu.vercel.app/"
+    return "https://book-app-backend-env-updated.vercel.app/"
 }
 
 export default getBaseUrl;
